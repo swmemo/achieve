121316fc06f1ba5f3cef27f151dacda8aaed5d6a
swmemo <s.watanabes@gmail.com>
2017-02-12 11:29:02 +0000

    DIVE09_1

--- Gemfile
+++ Gemfile
@@ -36,6 +36,15 @@ gem 'twitter-bootstrap-rails'
 
 gem 'jquery-turbolinks'
 
+# DIVE08のログイン学習用
+gem 'devise'
+
+# DIVE08の日本語化用
+gem 'rails-i18n'
+
+# DIVE09のテストデータ生成用
+gem 'faker'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,7 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:edit, :update, :destroy]
-
+  before_action :authenticate_user!
+  
   def index
     @blogs = Blog.all
   end
@@ -20,6 +21,7 @@ class BlogsController < ApplicationController
 
   def create
     @blog = Blog.new(blogs_params)
+    @blog.user_id = current_user.id
     if @blog.save
       # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
       redirect_to blogs_path, notice: "ブログを作成しました！"
