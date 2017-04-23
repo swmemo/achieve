400ccb4af135a983d29d47e17cd67bff7c387af9
swmemo <s.watanabes@gmail.com>
2017-04-23 04:26:39 +0000

    dive12_4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # actionmailer_setting (dive11)
-  config.action_mailer.default_url_options = { host: 'stormy-shelf-64762.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: "stormy-shelf-64762.herokuapp.com" }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
